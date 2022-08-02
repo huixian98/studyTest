fca9fa1b518acf02668746effcaf75713b5deb47
huhuixian1 <huhuixian1@xiaomi.com>
2022-08-02 19:40:33 +0800

    将test合并到staging部署测试环境，学习处理冲突

--- src/main/java/com/itstyle/seckill/Application.java
+++ src/main/java/com/itstyle/seckill/Application.java
@@ -21,8 +21,7 @@ public class Application {
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
 		LOGGER.info("项目启动 ");
-		System.out.println("修改测试test");
-		System.out.println("测试如何提交2");
+		System.out.println("将test合并到staging部署测试环境，学习处理冲突");
 
 	}
 }
