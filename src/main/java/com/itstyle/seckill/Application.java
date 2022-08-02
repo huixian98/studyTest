e933b8be68ffb11fa3fd4ad36bdd3915680a10ce
huhuixian1 <huhuixian1@xiaomi.com>
2022-08-02 19:13:29 +0800

    随便测试

--- src/main/java/com/itstyle/seckill/Application.java
+++ src/main/java/com/itstyle/seckill/Application.java
@@ -21,5 +21,7 @@ public class Application {
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
 		LOGGER.info("项目启动 ");
+		System.out.println("修改测试test");
+
 	}
 }
